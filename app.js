ca58a4d33abdc5112f9adc62d7d8936c0fd411ef
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-06 09:26:29 +0430

    init

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const btns = document.querySelectorAll('.btn')
 btns.forEach(function(btn) {
     btn.addEventListener('click', function(e) {
         const styles = e.currentTarget.classList;
-        if(styles.contains('decrease'))
+        if(styles.contains('decrease')) {
+            count--;
+        }
     })
 })
