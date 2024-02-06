a01aa879dc6d4f6889165a6ede51d9fe0a8764f5
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-06 09:41:11 +0430

    init

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ btns.forEach(function(btn) {
             value.style.color = 'red'
         }
         if(count === 0) {
-            value.style.color = 'black'
+            value.style.color = '#222'
         }
         value.textContent = count;
     })
