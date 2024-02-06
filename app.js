8757bd80174ecfcccde6117e93db49a765fc6fd9
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-06 09:22:07 +0430

    init

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const value = document.querySelector('#value')
 const btns = document.querySelectorAll('.btn')
 
 btns.forEach(function(btn) {
-    btn.addEventListener('click', function() {
-        
+    btn.addEventListener('click', function(e) {
+        console.log(e.currentTarget)
     })
 })
