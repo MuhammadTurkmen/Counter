7af9daff4cb4763c36c8a3ccde4e937d3c5443f3
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-06 09:20:14 +0430

    init

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ let count = 0;
 const value = document.querySelector('#value')
 const btns = document.querySelectorAll('.btn')
 
-btns.forEach(function(item) {
-    console.log(item)
+btns.forEach(function(btn) {
+    
 })
